5a9a22054440d6837c7bdbed8b95e538bd46e2f6
Hanvalad <hanvaladbabaev@yandex.ru>
2025-10-24 15:28:29 +0300

    Добавлена автоматическая валидация User и Film через аннотации @Valid

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,62 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> findFilms() {
+        log.info("Получен запрос GET /films. Количество фильмов сейчас: {}", films.size());
+        return films.values();
+    }
+
+    @PostMapping
+    public Film createFilms(@RequestBody Film film) {
+        log.info("Попытка создать фильм: {}", film);
+
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+
+        log.info("Фильм успешно создан: id={}, name='{}'", film.getId(), film.getName());
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilms(@RequestBody Film film) {
+        log.info("Попытка обновить фильм: {}", film);
+
+        if (film.getId() == null || !films.containsKey(film.getId())) {
+            log.warn("Обновление не удалось: фильм с id={} не найден", film.getId());
+            throw new ValidationException("Фильм с таким ID не найден для обновления");
+        }
+
+        films.put(film.getId(), film);
+
+        log.info("Фильм успешно обновлён: id={}, name='{}'", film.getId(), film.getName());
+        return film;
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return currentMaxId + 1;
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,36 @@
+package ru.yandex.practicum.filmorate.model;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.time.LocalDate;
+
+/**
+ * User.
+ */
+
+@Data
+public class User {
+    private Long id;
+
+    @Email(message = "Некорректный формат email")
+    @NotBlank(message = "Email не может быть пустым")
+    private String email;
+
+    @NotBlank(message = "Логин не может быть пустым или содержать пробелы")
+    private String login;
+
+    @NotBlank(message = "Имя не может быть пустым")
+    private String name;
+
+    @NotNull(message = "Дата рождения обязательна")
+    @PastOrPresent(message = "Дата рождения не может быть в будущем")
+    private LocalDate birthday;
+
+
+}
