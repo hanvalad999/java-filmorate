90d818503b8b87d01904d546c3257a27e58395dc
Hanvalad <hanvaladbabaev@yandex.ru>
2025-11-12 01:54:11 +0300

    style: исправлен пробел перед '{' в InMemoryUserStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -61,7 +61,7 @@ public class InMemoryUserStorage implements UserStorage {
     }
 
     @Override
-    public List<User> getMutualFriends(Integer userId, Integer friendId){
+    public List<User> getMutualFriends(Integer userId, Integer friendId) {
         List<User> mutualFriends = new ArrayList<>();
         for (Integer id :getUserById(userId).getFriends()) {
             if (getUserById(friendId).getFriends().contains(id)) {
