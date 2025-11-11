85a71c3e5c3eb6d0952e8d6524c2d0467f865421
Hanvalad <hanvaladbabaev@yandex.ru>
2025-11-12 02:03:31 +0300

    изменение тест UserControllerTest

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,141 +1,52 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserControllerTest {
+    UserController controller;
+    UserStorage userStorage;
+    UserService userService;
+    User testUser;
 
-    private final UserController controller = new UserController();
+    @BeforeEach
+    protected void init() {
+        userStorage = new InMemoryUserStorage();
+        userService = new UserService(userStorage);
+        controller = new UserController(userStorage, userService);
 
-    @Test
-    void createUsers_nullBody_shouldThrow() {
-        assertThrows(
-                ValidationException.class,
-                () -> controller.createUsers(null),
-                "Ожидалось, что null-тело не пройдёт валидацию"
-        );
+        testUser = User.builder()
+                .name("John")
+                .email("john@mail.ru")
+                .login("login")
+                .birthday(LocalDate.of(1987, 4, 14))
+                .build();
+
     }
 
     @Test
-    void createUsers_emptyEmail_shouldThrow() {
-        User user = new User();
-        user.setEmail("   ");     // пусто
-        user.setLogin("login");
-        user.setName("name");
-        user.setBirthday(LocalDate.of(2000, 1, 1));
-
-        assertThrows(
-                ValidationException.class,
-                () -> controller.createUsers(user),
-                "Пустой email должен привести к ошибке"
-        );
+    public void createUser_NameIsBlank_NameIsLoginTest() {
+        testUser.setName("");
+        controller.create(testUser);
+        assertEquals("login", controller.getUsers().get(0).getName());
     }
 
     @Test
-    void createUsers_emailWithoutAt_shouldThrow() {
-        User user = new User();
-        user.setEmail("mail.example.com");  // без @
-        user.setLogin("login");
-        user.setName("name");
-        user.setBirthday(LocalDate.of(2000, 1, 1));
-
-        assertThrows(
-                ValidationException.class,
-                () -> controller.createUsers(user),
-                "Email без @ не должен проходить"
-        );
-    }
-
-    @Test
-    void createUsers_emptyLogin_shouldThrow() {
-        User user = new User();
-        user.setEmail("test@example.com");
-        user.setLogin("   "); // пустой логин
-        user.setName("name");
-        user.setBirthday(LocalDate.of(2000, 1, 1));
-
-        assertThrows(
-                ValidationException.class,
-                () -> controller.createUsers(user),
-                "Пустой логин не должен проходить"
-        );
-    }
-
-    @Test
-    void createUsers_loginWithSpaces_shouldThrow() {
-        User user = new User();
-        user.setEmail("test@example.com");
-        user.setLogin("bad login"); // с пробелом
-        user.setName("name");
-        user.setBirthday(LocalDate.of(2000, 1, 1));
-
-        assertThrows(
-                ValidationException.class,
-                () -> controller.createUsers(user),
-                "Логин с пробелом не должен проходить"
-        );
-    }
-
-    @Test
-    void createUsers_futureBirthday_shouldThrow() {
-        User user = new User();
-        user.setEmail("test@example.com");
-        user.setLogin("user");
-        user.setName("User Name");
-        user.setBirthday(LocalDate.now().plusDays(1)); // завтра = будущее
-
-        assertThrows(
-                ValidationException.class,
-                () -> controller.createUsers(user),
-                "Дата рождения в будущем не должна проходить"
-        );
-    }
-
-    @Test
-    void createUsers_todayBirthday_shouldPass() {
-        User user = new User();
-        user.setEmail("test@example.com");
-        user.setLogin("user");
-        user.setName("User Name");
-        user.setBirthday(LocalDate.now()); // сегодня ок
-
-        assertDoesNotThrow(
-                () -> controller.createUsers(user),
-                "Дата рождения сегодня должна быть допустима"
-        );
-    }
-
-    @Test
-    void createUsers_emptyName_shouldUseLoginAsName() {
-        User user = new User();
-        user.setEmail("test@example.com");
-        user.setLogin("vasya");
-        user.setName("   "); // имя пустое
-        user.setBirthday(LocalDate.of(1990, 5, 20));
-
-        User created = controller.createUsers(user);
-
-        assertEquals("vasya", created.getName(),
-                "Если имя пустое, оно должно подставиться из логина");
-    }
-
-    @Test
-    void createUsers_validUser_shouldAssignIdAndStore() {
-        User user = new User();
-        user.setEmail("test@example.com");
-        user.setLogin("neo");
-        user.setName("Thomas Anderson");
-        user.setBirthday(LocalDate.of(1990, 3, 1));
-
-        User created = controller.createUsers(user);
-
-        assertNotNull(created.getId(), "После сохранения у пользователя должен быть id");
-        assertEquals(1L, created.getId(), "Первый пользователь должен получить id=1");
-        assertEquals("neo", created.getLogin());
+    void createUser_BirthdayInFuture_badRequestTest()  {
+        testUser.setBirthday(LocalDate.parse("2024-10-12"));
+        try{
+            controller.create(testUser);
+        } catch (ValidationException e){
+            assertEquals("Неверно указана дата рождения", e.getMessage());
+        }
     }
 }
