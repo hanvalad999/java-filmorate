7ed79a90f509fd7603256c0f22c7972804fb6112
Hanvalad <hanvaladbabaev@yandex.ru>
2025-12-02 19:31:55 +0300

    Удалить неиспользуемый импорт компаратора в FilmService

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.storage.MpaStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.time.LocalDate;
-import java.util.Comparator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Optional;
